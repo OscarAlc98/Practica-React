ed9dcd2b3a3df963b1d07608cc7ca2eb1963b207
OscarAlc98 <oscaralc98@gmail.com>
2022-04-18 20:26:35 -0500

    React tarea

---
+++ pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./Components/Main";
+import './Components/style.css'
+function App() {
+  return (
+    <>
+      <h1></h1>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
